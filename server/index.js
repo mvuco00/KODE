14ff58de15f25dcd0410c2ab185f13c3f1cf3391
mvuco00 <mvuco00@fesb.hr>
2020-12-28 17:58:59 +0100

    povezivanje na bazu

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+
+//postavljanje bodyParsera kako bi mogao ispravno slati zahtjeve
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://userKODE:marija2020@clusterkode.28tl0.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+//connect vraća promise
+//call back u then-u se pokrene kad je spajanje uspješno
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
