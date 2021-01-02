fff45ba6f74538ac9baa428d7e2a9cb065cd6204
mvuco00 <mvuco00@fesb.hr>
2021-01-02 21:39:10 +0100

    prikaz postova

--- client/kode/src/components/Post.js
+++ client/kode/src/components/Post.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Post = () => {
+const Post = ({ post }) => {
+  console.log(post);
   return (
     <div className="post">
-      <h1>POST</h1>
+      <h1>{post.title}</h1>
+      <h5>{post.message}</h5>
+      <span>by: {post.creator}</span>
+      <span>likes: {post.likes}</span>
     </div>
   );
 };
--- client/kode/src/components/Posts.js
+++ client/kode/src/components/Posts.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Post from "./Post";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
@@ -9,7 +10,17 @@ const Posts = () => {
     <div className="posts">
       <h3>POSTS</h3>
       <div>
-        <Post />
+        {!posts.length ? (
+          <CircularProgress />
+        ) : (
+          <Grid container alignItems="stretch" spacing={3}>
+            {posts.map((post) => (
+              <Grid key={post._id} item xs={12} sm={6}>
+                <Post post={post} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </div>
     </div>
   );
