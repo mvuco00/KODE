5040683a87312e0f9794ae273cdd685c6cfca815
mvuco00 <mvuco00@fesb.hr>
2021-01-03 00:12:56 +0100

    search component

--- client/kode/src/components/Search.js
+++ client/kode/src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Search = () => {
   return (
-    <input
-      type="text"
-      placeholder="What are you looking for?"
-      className="search"
-    />
+    <div className="search">
+      <input type="text" placeholder="What are you looking for?" />
+      <SearchIcon style={{ color: "green" }} />
+    </div>
   );
 };
 
