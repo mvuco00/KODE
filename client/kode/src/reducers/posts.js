bf7491f10fdacbb68f3c78c6179c3e71d4d2c2b4
mvuco00 <mvuco00@fesb.hr>
2021-02-21 16:50:43 +0100

    action types

--- client/kode/src/reducers/posts.js
+++ client/kode/src/reducers/posts.js
@@ -1,18 +1,25 @@
+import {
+  FETCH_ALL,
+  CREATE,
+  UPDATE,
+  LIKE,
+  DELETE,
+} from "../actionTypes/actionTypes";
 //state ce bit postovi, a to je array
 export default (state = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return state.filter((post) => post._id !== action.payload);
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...state, action.payload];
-    case "UPDATE":
+    case UPDATE:
       return state.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
 
-    case "LIKE":
+    case LIKE:
       return state.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
