8bc9f2de94224058ff950fb8a027e7a23552e51c
mvuco00 <mvuco00@fesb.hr>
2021-02-21 17:00:47 +0100

    env

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv"; // enviromental variables
 
 const app = express(); //poziv express funkcije kojim se radi express aplikcija
-
+dotenv.config();
 //postavljanje bodyParsera kako bi mogao ispravno slati zahtjeve
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -15,14 +16,15 @@ app.use(cors());
 //svim routama dodajemo postfix /posts
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://userKODE:marija2020@clusterkode.28tl0.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 //connect vraća promise
 //call back u then-u se pokrene kad je spajanje uspješno
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
